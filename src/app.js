f6d5c6669f65d822bf295b5b3f3c01be01feafe7
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-04-26 11:43:15 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+// Importing products from products.json file
+const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
+
+//Middlewares
+app.use(express.json());
+
+// GET endpoint for sending the products to client by id
+//// Endpoint - /api/v1/products/:id
+
+module.exports = app;
