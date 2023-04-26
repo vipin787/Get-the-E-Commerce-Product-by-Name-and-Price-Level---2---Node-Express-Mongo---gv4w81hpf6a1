a69f72ffc92110527058948f2d2507fbf73180e0
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-04-26 12:07:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,22 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
 //Middlewares
 app.use(express.json());
+app.get("/api/v1/products/:id", (req, res)=>{
+    const product = products.find((el)=> el.id ===Number(req.params.id));
+    if(!product){
+        return res.status(404).json({
+            status: "fail",
+            message: "Product not found!",
+        });
+    }
+    res.status(200).json({
+       status: "success",
+       message: "Product fetched successfully",
+       data: {
+        product,
+       }, 
+    });
+});
 
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
